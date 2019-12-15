d28c321fdc0f83ac440ce73bfbb194648859d12f
Jesse Edwards <jesse.david.edwards.1987@gmail.com>
2019-12-14 21:36:11 -0600

    initial commit

---
+++ server.js
@@ -0,0 +1,27 @@
+// Dependencies
+// =============================================================
+const express = require("express");
+const path = require("path");
+const fs = require("fs");
+
+
+// Sets up the Express App
+// =============================================================
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+// Sets up the Express app to handle data parsing
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+app.get("/", function (req, res) { //lookup (req, res) again tonigh
+  res.sendFile(path.join(__dirname, "/Develop/public/index.html"));
+});
+
+app.get("/notes", function (req, res) {
+  res.sendFile(path.join(__dirname, "/Develop/public/notes.html"));
+});
+
+app.listen(PORT, function () {
+  console.log("App listening on PORT " + PORT);
+});
